99171fd1732f7b16e8f99a1f5cee5ea845903534
Gobinda988888 <gobinda988888@gmail.com>
2025-10-18 22:00:13 +0530

    Add example credentials and update gitignore

---
+++ cred.example.py
@@ -0,0 +1,11 @@
+﻿#Twilio Details
+account_sid = 'YOUR_TWILIO_ACCOUNT_SID_HERE'
+auth_token = 'YOUR_TWILIO_AUTH_TOKEN_HERE'
+tw ilionumber = 'YOUR_TWILIO_PHONE_NUMBER_HERE'
+
+#Bot Token
+API_TOKEN = 'YOUR_TELEGRAM_BOT_TOKEN_HERE'
+
+#ngrok URL
+callurl = 'YOUR_NGROK_URL_HERE'
+twilosmsurl = 'YOUR_NGROK_URL_HERE/sms'
